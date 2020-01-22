61cb4a5774a9015f3debea367a9621b293b534de
U.M. islam <u.m.hamidulislam@labs-mac.local>
2020-01-22 15:53:29 +0600

    api link add

--- APIParsingTask/ViewController.swift
+++ APIParsingTask/ViewController.swift
@@ -7,12 +7,35 @@
 //
 
 import UIKit
+struct Country: Decodable  {
+    
+    let name: String
+    let capital: String
+    let region: String
+    
+}
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = "https://restcountries.eu/rest/v2/all"
+        let ulrObj = URL (string: url)
+        URLSession.shared.dataTask(with: ulrObj!) {(data, responds, error) in
+            do {
+                var countries = try JSONDecoder().decode([Country].self, from:
+                    data!)
+                for country in countries {
+                    print(country.name + " : " + country.capital)
+                }
+                
+            } catch{
+                print("we got an error")
+                
+            }
+        }.resume()
+        
     }
 
 
