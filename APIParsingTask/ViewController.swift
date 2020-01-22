437e27ec746900df6ad5f76dac9b7077f6d5bc47
U.M. islam <u.m.hamidulislam@labs-mac.local>
2020-01-22 17:30:06 +0600

    btn add

--- APIParsingTask/ViewController.swift
+++ APIParsingTask/ViewController.swift
@@ -17,27 +17,47 @@ struct Country: Decodable  {
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textView: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let url = "https://restcountries.eu/rest/v2/all"
+        /*let url = "https://restcountries.eu/rest/v2/all"
         let ulrObj = URL (string: url)
         URLSession.shared.dataTask(with: ulrObj!) {(data, responds, error) in
             do {
-                var countries = try JSONDecoder().decode([Country].self, from:
-                    data!)
+                var countries = try JSONDecoder().decode([Country].self, from: data!)
                 for country in countries {
-                    print(country.name + " : " + country.capital)
+                    print(country.name + " : " + country.capital + ":" + country.name + ":" + country.region)
                 }
                 
             } catch{
                 print("we got an error")
                 
             }
-        }.resume()
+        }.resume()*/
         
     }
+    @IBAction func btn(_ sender: Any) {
+        let url = "https://restcountries.eu/rest/v2/all"
+        let ulrObj = URL (string: url)
+        URLSession.shared.dataTask(with: ulrObj!) {(data, responds, error) in
+            do {
+                var countries = try JSONDecoder().decode([Country].self, from: data!)
+                for country in countries {
+                    print(country.name + " : " + country.capital + ":" + country.name + ":" + country.region)
+                }
+                
+            } catch{
+                print("we got an error")
+                
+            }
+            }.resume()
+        self.textView.text = "Hamidul"
+        
+    }
+    }
+
+    
 
 
-}
 
